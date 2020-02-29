a1644b13070dd5fbea635feb3a6740ab4a8eb1a3
Jagruthi9 <60088124+Jagruthi9@users.noreply.github.com>
2020-02-29 02:52:15 -0800

    Add files via upload

---
+++ models/place-appointment.ts
@@ -0,0 +1,8 @@
+export class PlaceAppointment {
+    public name:string;
+    public address:string;
+    public city:string;
+    public package:string;
+    public TrainerPreference:string;
+    public phone:number;
+}
